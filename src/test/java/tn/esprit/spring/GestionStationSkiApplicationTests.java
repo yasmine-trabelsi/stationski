439bd928cda24253626443f035bbde718676d407
yasmine-trabelsi <126244083+yasmine-trabelsi@users.noreply.github.com>
2024-05-03 14:06:11 +0100

    Update GestionStationSkiApplicationTests.java

--- src/test/java/tn/esprit/spring/GestionStationSkiApplicationTests.java
+++ src/test/java/tn/esprit/spring/GestionStationSkiApplicationTests.java
@@ -3,11 +3,11 @@ package tn.esprit.spring;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
-class GestionStationSkiApplicationTests {
+//@SpringBootTest
+//class GestionStationSkiApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+	//@Test
+	//void contextLoads() {
+	//}
 
-}
+//}
